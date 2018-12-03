fb69874ca5afc375ad004a4261cb96972264da94
“Your <you@example.com>
2018-12-03 20:34:04 +0800

    Initial Commit

---
+++ KnoeledgeKing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KnoeledgeKing
+//
+//  Created by User01 on 2018/12/3.
+//  Copyright © 2018 00557146. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
